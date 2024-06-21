99b839145d90946c25de34fa5eced83c5c9b0988
Elder <isairuiz138@gmail.com>
2024-06-21 12:27:05 -0500

    principal.dart

--- lib/src/principal.dart
+++ lib/src/principal.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -32,93 +32,37 @@ class _ECommercePageState extends State<ECommercePage> {
     });
   }
 
-  List<Map<String, dynamic>> products = [
-    {'name': 'Nike Air Max 2090', 'price': 150.0, 'image': 'assets/nike1.png'},
-    {'name': 'Nike React Vision', 'price': 140.0, 'image': 'assets/nike2.png'},
-    {'name': 'Nike Air Max 270', 'price': 160.0, 'image': 'assets/nike3.png'},
-  ];
+  final List<String> cartItems = []; // Lista de productos en el carrito
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SHOPIN'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.camera_alt),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              child: PageView(
-                controller: _pageController,
-                children: [
-                  Image.asset('assets/nike1.png', fit: BoxFit.cover),
-                  Image.asset('assets/nike2.png', fit: BoxFit.cover),
-                  Image.asset('assets/nike3.png', fit: BoxFit.cover),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "New Arrivals",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 16),
-                  Container(
-                    height: 250,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: products.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 160,
-                          child: Card(
-                            child: Column(
-                              children: [
-                                Image.asset(products[index]['image'], height: 120),
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(products[index]['name'], style: TextStyle(fontWeight: FontWeight.bold)),
-                                      SizedBox(height: 8),
-                                      Text('\$${products[index]['price']}', style: TextStyle(color: Colors.purple)),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        title: Image.asset(
+          'assets/logo.jpg',
+          height: 200, // Ajusta el tamaño del logo según sea necesario
         ),
+        backgroundColor: Colors.white,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomeTab(cartItems: cartItems, onItemAdded: (item) {
+            setState(() {
+              cartItems.add(item);
+            });
+          }), // Página de Compras
+          CartTab(cartItems: cartItems), // Carrito
+          FeedTab(), // Feed
+          MessagesTab(), // Mensajes
+          UserTab(), // Usuario
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
           BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
         ],
@@ -126,6 +70,176 @@ class _ECommercePageState extends State<ECommercePage> {
         selectedItemColor: Colors.purple,
         unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+// HomeTab: Página de Compras
+class HomeTab extends StatefulWidget {
+  final List<String> cartItems;
+  final ValueChanged<String> onItemAdded;
+
+  HomeTab({required this.cartItems, required this.onItemAdded});
+
+  @override
+  _HomeTabState createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<bool> addedToCart = List<bool>.generate(9, (index) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red.withOpacity(0.7), // Color rojo opaco para el fondo del cuerpo
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.blue, // Color azul para el cuadro del producto
+                    elevation: 4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/product${index + 1}.jpg', // Imagen del producto
+                          height: 350, // Ajusta el tamaño de la imagen según sea necesario
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: addedToCart[index]
+                              ? null
+                              : () {
+                            setState(() {
+                              addedToCart[index] = true;
+                              widget.onItemAdded('product ${index + 1}');
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red, // Color rojo para el botón
+                          ),
+                          child: Text(
+                            addedToCart[index] ? 'Agregado' : 'Agregar al Carrito', // Texto del botón
+                            style: TextStyle(
+                              color: Colors.white, // Color blanco para el texto del botón
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  addedToCart = List<bool>.generate(9, (index) => false); // Reiniciar los botones a su estado original
+                  widget.cartItems.clear();
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red, // Color rojo para el botón
+              ),
+              child: Text(
+                'Descartar Compra',
+                style: TextStyle(
+                  color: Colors.white, // Color blanco para el texto del botón
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// CartTab: Carrito
+class CartTab extends StatelessWidget {
+  final List<String> cartItems;
+
+  CartTab({required this.cartItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return CartPage(cartItems: cartItems);
+  }
+}
+
+// CartPage: Página del carrito de compras
+class CartPage extends StatelessWidget {
+  final List<String> cartItems;
+
+  CartPage({required this.cartItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carrito de Compras'),
+      ),
+      body: cartItems.isEmpty
+          ? Center(
+        child: Text('El carrito está vacío'),
+      )
+          : ListView.builder(
+        itemCount: cartItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(cartItems[index]),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// FeedTab: Feed
+class FeedTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Feed'),
+      ),
+    );
+  }
+}
+
+// MessagesTab: Mensajes
+class MessagesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Messages'),
+      ),
+    );
+  }
+}
+
+// UserTab: Usuario
+class UserTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('User'),
       ),
     );
   }
